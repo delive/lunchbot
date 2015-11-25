61963ab568ec9c385fa56f2388e866f4215f5b26
john.wright <john.wright@workday.com>
2015-11-24 21:47:17 -0800

    attempt to fix mysql timeouts

--- lunchbot.js
+++ lunchbot.js
@@ -1,40 +1,53 @@
 var mysql = require("mysql");
 var config = require("../config.js");
+var con;
 
-function connectMysql() {
-	var con = mysql.createConnection({
-	  host: config.mysql.host,
-	  user: config.mysql.user,
-	  password: config.mysql.password,
-	  database: config.mysql.database,
-	  multipleStatements: true
-});
+// function connectMysql() {
+// 	var con = mysql.createConnection({
+// 	  host: config.mysql.host,
+// 	  user: config.mysql.user,
+// 	  password: config.mysql.password,
+// 	  database: config.mysql.database,
+// 	  multipleStatements: true
+// });
 
-con.connect(function(err){
-  if(err){
-    console.log('Error connecting to Db');
-    return;
-  }
-  console.log('Mysql connection established');
-});
-return con;
-}
+// con.connect(function(err){
+//   if(err){
+//     console.log('Error connecting to Db');
+//     return;
+//   }
+//   console.log('Mysql connection established');
+// });
+// return con;
+// }
 
-function handleDisconnect(connection) {
-  connection.on('error', function(err) {
-    if (!err.fatal) {
-      return;
+function handleDisconnect() {
+
+  con = mysql.createConnection({
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database,
+    multipleStatements: true
+  });
+
+   // Recreate the connection, since
+   // the old one cannot be reused.
+
+  con.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  con.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
     }
-
-    if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
-      throw err;
-    }
-
-    console.log('Re-connecting lost connection: ' + err.stack);
-
-    connection = mysql.createConnection(connection.config);
-    handleDisconnect(connection);
-    connection.connect();
   });
 }
 
@@ -146,10 +159,10 @@ autoMark = true;
 
 slack = new Slack(token, autoReconnect, autoMark);
 
-con = connectMysql();
+// con = connectMysql();
 
 // node.js will hold a single connection to mysql. this will eventually timeout - just retry after
-handleDisconnect(con);
+handleDisconnect();
 
 slack.on('message', function(message) {
 
